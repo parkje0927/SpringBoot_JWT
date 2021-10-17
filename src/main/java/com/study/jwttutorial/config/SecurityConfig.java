aef56a07684075d6f84c58fcc134df869d5f5fb7
jh <parkje09270@gmail.com>
2021-10-17 14:25:10 +0900

    JWT 코드, Security 설정 추가

--- src/main/java/com/study/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/study/jwttutorial/config/SecurityConfig.java
@@ -1,12 +1,41 @@
 package com.study.jwttutorial.config;
 
+import com.study.jwttutorial.jwt.JwtAccessDeniedHandler;
+import com.study.jwttutorial.jwt.JwtAuthenticationEntryPoint;
+import com.study.jwttutorial.jwt.JwtSecurityConfig;
+import com.study.jwttutorial.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //@PreAuthorize 를 메소드 단위로 추가하기 위해서 사용
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -15,12 +44,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**", "/favicon.ico");
     }
 
+    /**
+     * - 토큰을 사용하기 때문에 csrf 설정은 disable
+     * - 세션을 사용하지 않아서 세션 설정은 stateless
+     * - 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+     * @param http
+     * @throws Exception
+     */
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/hello").permitAll() //이 요청은 허락하고,
-                .anyRequest().authenticated(); //나머지 요청은 인증받아야 한다.
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
 
+                .anyRequest().authenticated() //나머지 요청은 인증받아야 한다.
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
     }
 }
---
+++ src/main/java/com/study/jwttutorial/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.study.jwttutorial.jwt;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    /**
+     * 필요한 권한이 존재하지 않은 경우에 403 Forbidden 에러를 리턴하는 클래스
+     */
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
